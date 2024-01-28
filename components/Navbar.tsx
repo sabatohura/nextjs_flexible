566b95d0cf5d93b9fd11a454c1faea04c81f1aee
sabatohura <sabato@hura.one>
2024-01-28 15:30:30 +0200

    Complete Footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,9 @@
+import { NavLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import AuthProviders from "./AuthProviders";
 const Navbar = () => {
+  const session = {};
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
@@ -8,7 +11,23 @@ const Navbar = () => {
           <Image src="/logo.svg" alt="Flexible" width={115} height={43} />
         </Link>
       </div>
-      <ul className="xl:flex hidden text-small gap-7"></ul>
+      <ul className="xl:flex hidden text-small gap-7">
+        {NavLinks.map((link) => (
+          <Link key={link.key} href={link.href}>
+            <li>{link.text}</li>
+          </Link>
+        ))}
+      </ul>
+      <div className="flexCenter gap-4">
+        {session ? (
+          <>
+            User Photo
+            <Link href="/create-project">Share Work</Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
     </nav>
   );
 };
