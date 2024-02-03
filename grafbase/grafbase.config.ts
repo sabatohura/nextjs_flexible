09266af41335108478e51492a24c24a9a41c85d2
sabatohura <sabato@hura.one>
2024-02-04 01:37:57 +0200

    Add Graph base

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,38 @@
+import { graph, config } from "@grafbase/sdk";
+
+const g = graph.Standalone();
+
+// Data Sources - https://grafbase.com/docs/connectors
+//
+// const pg = connector.Postgres('pg', { url: g.env('DATABASE_URL') })
+// g.datasource(pg)
+
+// Resolvers - https://grafbase.com/docs/resolvers
+//
+// g.query('helloWorld', {
+//   returns: g.string(),
+//   resolver: 'hello-world',
+// })
+
+export default config({
+  graph: g,
+  // Authentication - https://grafbase.com/docs/auth
+  auth: {
+    // OpenID Connect
+    // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
+    // providers: [oidc],
+    rules: (rules) => {
+      rules.public();
+    },
+  },
+  // Caching - https://grafbase.com/docs/graphql-edge-caching
+  // cache: {
+  //   rules: [
+  //     {
+  //       types: ['Query'], // Cache everything for 60 seconds
+  //       maxAge: 60,
+  //       staleWhileRevalidate: 60
+  //     }
+  //   ]
+  // }
+});
